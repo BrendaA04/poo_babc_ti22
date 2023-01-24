ca552ac3291b29ab1079121b5c35f9dd3b6eb533
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-01-24 14:05:00 +0000

    Realizamos errores de sintaxis para conocer los errores mas comunes

---
+++ semana2/programa2.py
@@ -0,0 +1,13 @@
+"""
+    programa1
+    Nombre:Brenda ABC
+    Fecha: 24/01/2023
+    Descripcion:
+    invocando variables utilizando llaves.
+    
+
+"""
+variable1= "Hola"  #  variable para almacenar una cadena de caracteres
+variable2=  619  #  variable para almacenar una cadena de caracteres 
+#print(variable1 + variable2)  #  lee variables e imprime resultado
+print("{} {}".format(variable1    ,    variable2))  #  lee variables e imprime resultado
