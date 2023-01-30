d57d8543d68c2bdad99107eae5083acc38bef849
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-01-30 14:56:09 +0000

    Cambiar tipos de datos

---
+++ semana3/programa4.py
@@ -0,0 +1,17 @@
+numero1 = input("Numero1: ")
+numero2 = 10
+
+suma = int (numero1) + numero2
+print(suma)
+
+multiplicacion = int (numero1) * numero2
+print(multiplicacion)
+
+resta = int (numero1) - numero2
+print(resta)
+
+division = int (numero1) / numero2
+print(division)
+
+potencia = int (numero1) ** numero2
+print(potencia)
