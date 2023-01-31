0af76a8e393901bb6bb3e5dba3cbd58bbf4721a5
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-01-31 04:35:50 +0000

    Se realizó de tarea una aplicación qué te permita calcular el área y perímetro de cualquier triángulo

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -6,19 +6,18 @@
     PERÍMETRO Y ÁREA DE UN TRIANGULO
     
 """
-
-
 print(" ÁREA Y PERÍMETRO DEL TRIÁNGULO.")
 ##UN PRINT PARA QUE SE DE ESPACIO DE UNA LINEA 
 print()
-print("ÁREA DEL TRIÁNGULO")
+
 b = int(input("INGRESE BASE   : "))
 h = int(input("INGRESE ALTURA : "))
-l1 = int(input("INGRESE LA MEDIDA DE EL LADO : "))
+l1 = int(input("INGRESE LA MEDIDA DE UN LADO : "))
+l2 = int(input("INGRESA LA MEDIDA DE EL LADO QUE RESTA:"))
 ##PARA SACAR EL ÁREA 
 A = (b*h)/2
 ##PARA SACAR EL PERIMETRO
-P = l1*3
+P = b + l1 + l2
 print()
 print("PERIMETRO : ", P)
 print("ÁREA : ", A)
