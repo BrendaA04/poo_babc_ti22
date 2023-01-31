a2a73b4eda5e8242669752727c8fe9390b001d4a
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-01-31 00:12:32 +0000

    CAMBIAR TIPO DE DATO CON int

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -1,3 +1,12 @@
+"""
+    programa5
+    Nombre:Brenda ABC
+    Fecha: 30/01/2023
+    Descripcion:
+    PERÍMETRO Y ÁREA DE UN TRIANGULO
+    
+"""
+
 
 print(" ÁREA Y PERÍMETRO DEL TRIÁNGULO.")
 ##UN PRINT PARA QUE SE DE ESPACIO DE UNA LINEA
