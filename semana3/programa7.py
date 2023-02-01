ba172e834257063c0feabcd1a0fc63c6bc79342e
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-01 14:52:22 +0000

    Comparar 2 números enteros e imprimir el numero mayor

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -8,10 +8,10 @@
 """
 print("Comparar 2 números enteros e imprimir el número mayor")
 print()
-
+##SE LE PIDE AL USUARIO QUE INGRESE EL VALOR DE DOS NUMEROS POSITIVOS O NEGATIVOS
 numero_1 = int(input("INGRESE EL VALOR DE EL PRIMER NÚMERO : "))
 numero_2 = int(input("INGRESE EL VALOR DE EL SEGUNDO NÚMERO : "))
-
+#SE HACE LA COMPARACION DE LOS NUMEROS INGRESADOS Y TE DARA EL VALOR DE QUE NUMERO ES MAYOR 
 if numero_1 > numero_2:  
     print(numero_1)
 else:
