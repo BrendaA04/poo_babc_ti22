0248eddc23ac4541233ae82fc01f94c99c5303cb
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-01 14:50:07 +0000

    Comparar 2 números enteros e imprimir el numero mayor

---
+++ semana3/programa7.py
@@ -0,0 +1,18 @@
+"""
+    programa5
+    Nombre:Brenda ABC
+    Fecha: 01/02/2023
+    Descripcion:
+    Comparar 2 numeros enteros e imprimir el numero mayor
+    
+"""
+print("Comparar 2 números enteros e imprimir el número mayor")
+print()
+
+numero_1 = int(input("INGRESE EL VALOR DE EL PRIMER NÚMERO : "))
+numero_2 = int(input("INGRESE EL VALOR DE EL SEGUNDO NÚMERO : "))
+
+if numero_1 > numero_2:  
+    print(numero_1)
+else:
+    print(numero_2)
