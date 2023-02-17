0639618a41eb170d4f29b052af2b492acf35038e
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-17 04:09:48 +0000

    Se agregaron comentarios de linea

--- semana3/programa7.py
+++ semana3/programa7.py
@@ -12,9 +12,9 @@ print()
 numero_1 = int(input("INGRESE EL VALOR DE EL PRIMER NÚMERO : "))
 numero_2 = int(input("INGRESE EL VALOR DE EL SEGUNDO NÚMERO : "))
 #SE HACE LA COMPARACION DE LOS NUMEROS INGRESADOS Y TE DARA EL VALOR DE QUE NUMERO ES MAYOR 
-if numero_1 > numero_2:  
-    print(numero_1)
-elif numero_2 > numero_1:
-    print(numero_2)
-else:
-    print(None)
+if numero_1 > numero_2:  #permite ejecutar el código Python si se cumple una condición.
+    print(numero_1)#si se cumple la condicion antes establecida se imprime esta linea 
+elif numero_2 > numero_1: #se utiliza para comprobar múltiples condiciones si una condición es falsa, evalúa múltiples condiciones
+    print(numero_2)#si se cumple la condicion antes establecida se imprime esta linea 
+else:  ##Ejecutan una o varias sentencias de manera condicional. Puede utilizar una sintaxis de una sola línea o varias líneas en un bloque.
+    print(None)#si se no cumple la condicion antes establecida se imprime esta linea 
