1a6529f6c10293db490ab9c65e8bfa06fca53907
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-17 03:27:41 +0000

    SE AGREGARON LINEAS DE COMENTARIOS

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -9,10 +9,10 @@
 variable1= 10
 variable2= 5
 #print(variable1 + variable2 )  #  la primera lo suma y la otra lo concatena
-print("{} + {} = {}".format(variable1,variable2, (variable1 + variable2)))
+print("{} + {} = {}".format(variable1,variable2, (variable1 + variable2))) #	Formatea cadenas de texto
 #Crear diccionarios,Formatear cadenas de texto
-print("{} - {} = {}".format(variable1,variable2, (variable1 - variable2)))
+print("{} - {} = {}".format(variable1,variable2, (variable1 - variable2)))#las llaves lo que hacen es formatear cadenas 
 
-print("{} = {} * {}".format(( variable1 * variable2),variable1,variable2))
+print("{} = {} * {}".format(( variable1 * variable2),variable1,variable2))#las llaves almacenan informacion no visible hasta que despues la ingrese el usuario
 
-print("El resultado de dividir {} / {} = {}".format(variable1,variable2, (variable1 / variable2)))
+print("El resultado de dividir {} / {} = {}".format(variable1,variable2, (variable1 / variable2)))#imprime los datos almacenados depues de que se realice la operacion
