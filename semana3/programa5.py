16a633c1b7a456e035539719acdc23178d8e7e2b
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-17 03:40:22 +0000

    SE AGRAGARON COMENTARIOS  LINEA A LINEA

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -10,14 +10,14 @@ print(" ÁREA Y PERÍMETRO DEL TRIÁNGULO.")
 ##UN PRINT PARA QUE SE DE ESPACIO DE UNA LINEA 
 print()
 
-b = int(input("INGRESE BASE   : "))
-h = int(input("INGRESE ALTURA : "))
-l1 = int(input("INGRESE LA MEDIDA DE UN LADO : "))
-l2 = int(input("INGRESA LA MEDIDA DE EL LADO QUE RESTA:"))
+b = int(input("INGRESE BASE   : "))# esperar que el usuario introduzca un dato almacenar este dato en una variable.
+h = int(input("INGRESE ALTURA : "))# esperar que el usuario introduzca un dato almacenar este dato en una variable.
+l1 = int(input("INGRESE LA MEDIDA DE UN LADO : "))# esperar que el usuario introduzca un dato almacenar este dato en una variable.
+l2 = int(input("INGRESA LA MEDIDA DE EL LADO QUE RESTA:"))# esperar que el usuario introduzca un dato almacenar este dato en una variable.
 ##PARA SACAR EL ÁREA 
-A = (b*h)/2
+A = (b*h)/2#se agrega la formula con la que se va a trabajar asignada en una variable
 ##PARA SACAR EL PERIMETRO
-P = b + l1 + l2
+P = b + l1 + l2##se agrega la formula y se almacena en una variable 
 print()
-print("PERIMETRO : ", P)
-print("ÁREA : ", A)
+print("PERIMETRO : ", P)#va a imprimir el resultado de la operacion y te dara el perimetro 
+print("ÁREA : ", A)#va a imprimir el area de acuerdo a los datos que se agregaron por el usuario
