0b427af390273673ae0ec95b41625613e0cc89c8
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-18 22:32:00 +0000

    se agregaron líneas de comando

--- semana4/programa9.py
+++ semana4/programa9.py
@@ -7,27 +7,27 @@
     
 """
 
-def mayor(numero1,numero2):
-    if numero1 > numero2:
-        print(numero1)
-    elif numero2 > numero1:
-        print(numero2)
-    else:
-        print(None)
+def mayor(numero1,numero2): #constructor de la clase profesor
+    if numero1 > numero2: #if es para establecer una condicion y que se ejecute cuando se cumple 
+        print(numero1) #imprime el resultado
+    elif numero2 > numero1: #se utiliza para comprobar múltiples condiciones si una condición es falsa
+        print(numero2) #imprime el resultado
+    else: #Ejecutan una o varias sentencias de manera condicional
+        print(None) #imprime el resultado
 
-mayor (3,2)
+mayor (3,2) #se 
 mayor (2,3)
 mayor (3,3)
 
 ##programa que por medio de def definimos mayor
 
-def mayor(numero1:int,numero2:int)->int:
+def mayor(numero1:int,numero2:int)->int: #constructor de la clase profesor
     mayor=None
-    if numero1 > numero2:
+    if numero1 > numero2:#if es para establecer una condicion y que se ejecute cuando se cumple 
         mayor=numero1
-    elif numero2 > numero1:
+    elif numero2 > numero1:#se utiliza para comprobar múltiples condiciones si una condición es falsa
         mayor = numero2
-    else:
+    else: #Ejecutan una o varias sentencias de manera condicional
         mayor=None
     return mayor
 
