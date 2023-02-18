32889470ed719ef508fbc77ed14a131bc128a5af
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-18 22:54:58 +0000

    Se agregaron líneas de comando

--- semana4/programa9.py
+++ semana4/programa9.py
@@ -21,19 +21,19 @@ mayor (3,3)
 
 ##programa que por medio de def definimos mayor
 
-def mayor(numero1:int,numero2:int)->int: #constructor de la clase profesor
+def mayor(numero1:int,numero2:int)->int: # construye la clase persona 
     mayor=None
     if numero1 > numero2:#if es para establecer una condicion y que se ejecute cuando se cumple 
         mayor=numero1
     elif numero2 > numero1:#se utiliza para comprobar múltiples condiciones si una condición es falsa
         mayor = numero2
-    else: #Ejecutan una o varias sentencias de manera condicional
+    else:#Ejecutan una o varias sentencias de manera condicional
         mayor=None
-    return mayor
+    return mayor#la instrucción "return" se utiliza dentro de una función para especificar el valor que debe devolver la función cuando se llama
 
-print(mayor(3,2))
-print(mayor(2,3))
-print(mayor(3,3))
+print(mayor(3,2))#de esos dos numero establecidos se imprime cual es el numero mayor 
+print(mayor(2,3))#de esos dos numero establecidos se imprime cual es el numero mayor 
+print(mayor(3,3))#de esos dos numero establecidos se imprime None porque cuando son iguales establecimos que imprimiera None 
 
 
 
