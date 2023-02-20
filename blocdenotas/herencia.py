150a95c13597c436bcb4d50ae0569361fac05f84
BrendaA04 <122801442+BrendaA04@users.noreply.github.com>
2023-02-20 03:29:30 +0000

    Se agregaron comentarios en las lineas

--- blocdenotas/herencia.py
+++ blocdenotas/herencia.py
@@ -1,27 +1,27 @@
 """
     programaPersona
     Nombre:Brenda ABC
-    Fecha: 13/02/2023
+    Fecha: 14/02/2023
     Descripcion:
-    Crear una clase alumno para crear objetos con atributos 
+    Crear una clase  persona y una clase de alumno que hereda los atributos y metodos de la clase persona  
     
 """
 
-class Persona:
-   def __init__(self):
-        __nombre = None
-        print("Persona")
+class Persona: #crea una clase persona 
+   def __init__(self): #contructor de la clase persona 
+        __nombre = None #crea una variable privada __nombre 
+        print("Persona")#imprime como texto Persona 
 
-class Alumno(Persona):
-    def __init__(self):
-        super().__init__()
-        print("Alumno")
+class Alumno(Persona): #crea la clase alumno que hereda la clase persona 
+    def __init__(self): #constructor de la clase alumno
+        super().__init__() #aqui llama al constructor de la clase persona 
+        print("Alumno")# va aimprimir el texto persona 
 
-objecto_persona = Persona()
-objecto_alumno = Alumno()
+objecto_persona = Persona() #crea un objeto de la clase persona 
+objecto_alumno = Alumno() #crea un objecto de la clase alumno 
 
-objecto_persona.__nombre = "Dejah Thoris"
-print(objecto_persona.__nombre)
+objecto_persona.__nombre = "Dejah Thoris" #Aqui asigna valor en la variable __nombre para el objeto persona 
+print(objecto_persona.__nombre) #imprime el valor en nombre 
 
-objecto_alumno.__nombre = "Jhon Carter"
-print(objecto_alumno.__nombre)        
+objecto_alumno.__nombre = "Jhon Carter" #Aqui se asigna un valor a la variable __nombre para el objeto __alumno
+print(objecto_alumno.__nombre)  #imprime el valor en __nombre  
